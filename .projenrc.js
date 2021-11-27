a00814986efa6159a02e900e41ab87b1b16c0ec1
Chris Yang <kimisme9386@gmail.com>
2021-11-27 05:13:12 +0000

    feat: upgrade cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,8 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.129.0',
+  cdkVersion: '1.132.0',
   defaultReleaseBranch: 'main',
-  name: 'demo-waf-solution',
+  name: 'test-awscdk-app',
 
-  // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                    /* Runtime dependencies of this module. */
-  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                 /* Build dependencies for this module. */
-  // packageName: undefined,      /* The "name" in package.json. */
-  // release: undefined,          /* Add release management to this project. */
 });
 project.synth();
